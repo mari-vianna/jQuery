839ff2739ec5a1d1410465ed1e24b751a8b3895a
Mariana Vianna <marianaavianna@gmail.com>
2023-03-30 11:29:12 -0300

    criacao site novo

---
+++ Site/js/custom.js
@@ -0,0 +1,141 @@
+/*
+* Função que mostra o Carrossel
+*/
+
+$(document).ready(function(){
+
+   $('.owl-carousel').owlCarousel();
+   
+   let titulos = $('h4') 
+   let itens = $('.featured-item') 
+   let destaques = $('#featured') 
+});
+
+/*
+* Evento do Subscribe Form
+*/
+
+$('#form-submit').on('click', function(e){
+   e.preventDefault()
+
+   if($('#Email').val() != ''){
+      $('#Email').animate({
+         opacity: "toggle",
+         top: "-50"
+      }, 500, function(){
+         console.log($(this).val())
+      })
+   }
+})
+
+/*
+* Modal: Sobre/Contato
+*/
+
+$('.nav-modal-open').on('click', function(e){
+   e.preventDefault();
+
+   let elem = $(this).attr('rel')
+
+      $('.modal-body').html($('#'+elem).html())
+      $('.modal-header h5.modal-title').html($(this).text())
+
+   let myModal = new bootstrap.Modal($('#modelId'))
+   myModal.show()
+})
+
+/*
+* Modal: Projetos
+*/
+
+$('.nav-galleryImg').on('click', function(e){
+   e.preventDefault();
+
+   let elem = $(this).attr('rel')
+
+      $('.modalBody').html($('#'+elem).html())
+      $('.modalHeader h5.modalTitle').html($(this).text())
+
+   let myModal = new bootstrap.Modal($('#gallery-popup'))
+   myModal.show()
+})
+
+/*
+* Validation
+*/
+
+function validate(elem){
+   if( elem.val() == '') {
+      console.log('O campo de '+ elem.attr('name') + ' é obrigatório.')
+
+      elem.parent().find('#textRequired').show()
+      elem.addClass('invalid');
+      $('#submitButton').prop('disabled', true)
+      return false
+   } else {
+      elem.parent().find('#textRequired').hide()
+      elem.removeClass('invalid')
+      $('#submitButton').prop('disabled', false)
+   }
+}
+
+$('body').on('submit', '.modal-body #myForm', function(e){
+   e.preventDefault()
+
+   const inputName = $('#name')
+   const inputEmail = $('#email')
+   const inputCep = $('#cep')
+   const inputCel = $('#celular')
+   const inputCpf = $('#cpf')
+
+   validate(inputName)
+   validate(inputEmail)
+   validate(inputCep)
+   validate(inputCel)
+   validate(inputCpf)
+
+   if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid') || inputCep.hasClass('invalid') || inputCel.hasClass('invalid') || inputCpf.hasClass('invalid')){
+      alert('Verificar campos obrigatórios.')
+      return false
+   } else {
+      $(this).submit()  
+   }
+
+})
+
+$('body').on('blur', '#Email', function(){
+   validate($(this))
+})
+$('body').on('blur', '#name', function(){
+   validate($(this))
+})
+$('body').on('blur', '#email', function(){
+   validate($(this))
+})
+$('body').on('blur', '#date', function(){
+   validate($(this))
+}) 
+$('body').on('blur', '#time', function(){
+   validate($(this))
+})
+$('body').on('blur', '#cep', function(){
+   validate($(this))
+})
+$('body').on('blur', '#celular', function(){
+   validate($(this))
+})
+$('body').on('blur', '#cpf', function(){
+   validate($(this))
+})
+
+/*
+* Masks
+*/
+
+$(document).ready(function(){
+   $('#date').mask('00/00/0000', {placeholder: '00/00/0000'});
+   $('#time').mask('00:00', {placeholder: '00:00'});
+   $('#cep').mask('00000-000', {placeholder: '00000-000'});
+   $('#celular').mask('(00)0 0000-0000',{placeholder: '(00)0 0000-0000'});
+   $('#cpf').mask('000.000.000-00', {reverse: true, placeholder:'000.000.000-00'});
+})
